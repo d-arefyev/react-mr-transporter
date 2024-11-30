177cd6e4196d1c7744fec07f1acaeb1f5bbab49e
Denis Arefyev <163993361+DenisArefyev@users.noreply.github.com>
2024-11-30 23:35:16 +0100

    corrected

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 import mainLogo from '../../assets/icons/main-logo.svg';
 import cartIcon from '../../assets/icons/cart.svg';
 import LoginButton from '../Buttons/LoginButton/LoginButton';
+
 import styles from './Header.module.scss';
 
 const Header = ({ cartItems = [] }) => {
@@ -29,39 +31,58 @@ const Header = ({ cartItems = [] }) => {
       <div className="container">
         <div className={styles.headerContent}>
           {/* Логотип */}
-          <Link to="/">
+          <NavLink to="/">
             <img src={mainLogo} alt="MR.TRANSPORTER" className={styles.mainLogo} />
-          </Link>
+          </NavLink>
 
           <div className={styles.headerRight}>
             {/* Навигация */}
             <nav className={styles.navBlock}>
               <ul>
                 <li>
-                  <Link to="/about">Über uns</Link>
+                  <NavLink
+                    to="/about"
+                    className={({ isActive }) => (isActive ? styles.activeLink : '')}
+                  >
+                    Über uns
+                  </NavLink>
                 </li>
                 <li>
-                  <Link to="/pickup">Fahrzeug abholen</Link>
+                  <NavLink
+                    to="/pickup"
+                    className={({ isActive }) => (isActive ? styles.activeLink : '')}
+                  >
+                    Fahrzeug abholen
+                  </NavLink>
                 </li>
                 <li>
-                  <Link to="/contact">Kontakt</Link>
+                  <NavLink
+                    to="/contact"
+                    className={({ isActive }) => (isActive ? styles.activeLink : '')}
+                  >
+                    Kontakt
+                  </NavLink>
                 </li>
                 <li>
-                  <Link to="/account">Mein Konto</Link>
+                  <NavLink
+                    to="/account"
+                    className={({ isActive }) => (isActive ? styles.activeLink : '')}
+                  >
+                    Mein Konto
+                  </NavLink>
                 </li>
               </ul>
             </nav>
-            
+
             <LoginButton onClick />
-            {/* <LoginButton onClick={() => alert("Button clicked!")} /> */}
 
             {/* Корзина */}
-            <Link to="/cart" className={styles.cartIcon}>
+            <NavLink to="/cart" className={styles.cartIcon}>
               <img src={cartIcon} alt="Cart" />
               {cartItemsCount > 0 && (
                 <span className={styles.cartBadge}>{cartItemsCount}</span>
               )}
-            </Link>
+            </NavLink>
 
             {/* Кнопка бургер для мобильного меню */}
             {isMobile && !isMenuOpen && (
@@ -76,31 +97,57 @@ const Header = ({ cartItems = [] }) => {
           </div>
         </div>
       </div>
+
       {/* Кнопка закрытия меню для мобильного устройства */}
       {isMobile && isMenuOpen && (
-              <button className={styles.closeButton} onClick={handleMenuToggle}>
-                <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
-                  <path d="M3.19995 28.8L28.7999 3.20001" stroke="#747270" strokeWidth="2.3" strokeLinecap="round" />
-                  <path d="M28.8 28.8L3.20005 3.20001" stroke="#747270" strokeWidth="2.3" strokeLinecap="round" />
-                </svg>
-              </button>
-            )}
+        <button className={styles.closeButton} onClick={handleMenuToggle}>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none">
+            <path d="M3.19995 28.8L28.7999 3.20001" stroke="#747270" strokeWidth="2.3" strokeLinecap="round" />
+            <path d="M28.8 28.8L3.20005 3.20001" stroke="#747270" strokeWidth="2.3" strokeLinecap="round" />
+          </svg>
+        </button>
+      )}
+
       {/* Мобильное меню */}
       {isMobile && isMenuOpen && (
         <div className={`${styles.mobileOverlay} ${isMenuOpen ? styles.visible : ''}`}>
           <div className={styles.mobileMenuBody}>
             <ul className={styles.mobileMenuList}>
               <li className={styles.mobileMenuItem}>
-                <Link to="/about" className={styles.navLink} onClick={handleMenuToggle}>Über uns</Link>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) => (isActive ? styles.activeLink : '')}
+                  onClick={handleMenuToggle}
+                >
+                  Über uns
+                </NavLink>
               </li>
               <li className={styles.mobileMenuItem}>
-                <Link to="/pickup" className={styles.navLink} onClick={handleMenuToggle}>Fahrzeug abholen</Link>
+                <NavLink
+                  to="/pickup"
+                  className={({ isActive }) => (isActive ? styles.activeLink : '')}
+                  onClick={handleMenuToggle}
+                >
+                  Fahrzeug abholen
+                </NavLink>
               </li>
               <li className={styles.mobileMenuItem}>
-                <Link to="/contact" className={styles.navLink} onClick={handleMenuToggle}>Kontakt</Link>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) => (isActive ? styles.activeLink : '')}
+                  onClick={handleMenuToggle}
+                >
+                  Kontakt
+                </NavLink>
               </li>
               <li className={styles.mobileMenuItem}>
-                <Link to="/account" className={styles.navLink} onClick={handleMenuToggle}>Mein Konto</Link>
+                <NavLink
+                  to="/account"
+                  className={({ isActive }) => (isActive ? styles.activeLink : '')}
+                  onClick={handleMenuToggle}
+                >
+                  Mein Konto
+                </NavLink>
               </li>
             </ul>
           </div>
@@ -111,121 +158,3 @@ const Header = ({ cartItems = [] }) => {
 };
 
 export default Header;
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// // import { useSelector } from 'react-redux';
-// import { Link } from "react-router-dom";
-
-// import mainLogo from "../../assets/icons/main-logo.svg";
-// import cartIcon from "../../assets/icons/cart.svg";
-// import LoginButton from "../Buttons/LoginButton/LoginButton";
-
-// import styles from "./Header.module.scss";
-
-// const Header = ({ cartItems = [] }) => {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false); 
-//   // const cartItems = useSelector((state) => state.cart.items);
-//   const cartItemsCount = cartItems.reduce((total, item) => total + item.quantity, 5);
-//   const [isMobile, setIsMobile] = useState(window.innerWidth <= 767);
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setIsMobile(window.innerWidth <= 767);
-//     };
-
-//     window.addEventListener('resize', handleResize);
-
-//     return () => window.removeEventListener('resize', handleResize);
-//   }, []);
-
-//   const handleMenuToggle = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
-//   const handleSubmit = () => {
-//     alert("Button clicked!");
-//   };
-
-//   return (
-//     <header>
-//       <div className="container">
-//         <div className={styles.headerContent}>
-//           {/* Логотип */}
-//           <Link to="/">
-//             <img src={mainLogo} alt="MR.TRANSPORTER" className={styles.mainLogo} />
-//           </Link>
-
-//           <div className={styles.headerRight}>
-//             {/* Навигация */}
-//             <nav className={styles.navBlock}>
-//               <ul>
-//                 <li>
-//                   <Link to="/about">Über uns</Link>
-//                 </li>
-//                 <li>
-//                   <Link to="/pickup">Fahrzeug abholen</Link>
-//                 </li>
-//                 <li>
-//                   <Link to="/contact">Kontakt</Link>
-//                 </li>
-//                 <li>
-//                   <Link to="/account">Mein Konto</Link>
-//                 </li>
-//               </ul>
-//             </nav>
-            
-//             <LoginButton
-//               // onClick={() => console.log("Login clicked")} 
-//               onClick
-//             />
-
-//             {/* Корзина */}
-//             <Link to="/cart" className={styles.cartIcon}>
-//               <img src={cartIcon} alt="Cart" />
-//               {cartItemsCount > 0 && (
-//                 <span className={styles.cartBadge}>{cartItemsCount}</span>
-//               )}
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-
-//       {isMobile && (
-//         <div className={`${styles.mobileOverlay} ${isMenuOpen ? styles.visible : ''}`}>
-//           <button className={styles.closeButton} onClick={handleMenuToggle}>
-//             <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-//               <path d="M34 2L2 34" stroke="none" strokeWidth="3.67" strokeLinecap="round" strokeLinejoin="round"/>
-//               <path d="M2 2L34 34" stroke="none" strokeWidth="3.67" strokeLinecap="round" strokeLinejoin="round"/>
-//             </svg>
-//           </button>
-//           <div className={styles.mobileMenuBody}>
-//             <ul className={styles.mobileMenuList}>
-//               <li className={styles.mobileMenuItem}>
-//                 <Link to="/about" className={styles.navLink} onClick={handleMenuToggle}>Über uns</Link>
-//               </li>
-//               <li className={styles.mobileMenuItem}>
-//                 <Link to="/pickup" className={styles.navLink} onClick={handleMenuToggle}>Fahrzeug abholen</Link>
-//               </li>
-//               <li className={styles.mobileMenuItem}>
-//                 <Link to="/contact" className={styles.navLink} onClick={handleMenuToggle}>Kontakt</Link>
-//               </li>
-//               <li className={styles.mobileMenuItem}>
-//                 <Link to="/account" className={styles.navLink} onClick={handleMenuToggle}>Mein Konto</Link>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       )}
-//     </header>
-//   );
-// };
-
-// export default Header;
